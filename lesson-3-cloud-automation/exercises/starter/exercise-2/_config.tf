a344b84abeee625c8f38a0873cc94672399b5579
Vyacheslav Khvan <vykhvan@gmail.com>
2022-11-06 13:43:25 +0600

    Update bucket

--- lesson-3-cloud-automation/exercises/starter/exercise-2/_config.tf
+++ lesson-3-cloud-automation/exercises/starter/exercise-2/_config.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "udacity-tf-emmanuel-student" # Update here with your S3 bucket
+    bucket = "udacity-tf-vyacheslav-class" # Update here with your S3 bucket
     key    = "terraform/lesson3-ex-2.tfstate"
     region = "us-east-2"
   }
